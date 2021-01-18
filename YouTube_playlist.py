08c7a732bacf6f94754db4ff57ed9c402fb9d0b8
Hesham <61054665+h9-tect@users.noreply.github.com>
2021-01-18 19:54:58 +0200

    Update YouTube_playlist.py

--- YouTube_playlist.py
+++ YouTube_playlist.py
@@ -1,5 +1,5 @@
 from pytube import Playlist
 
-playlist = input (" paste your list")
+playlist = input (" paste your list") : 
 print('Number of videos in playlist: %s' % len(playlist.video_urls))
 playlist.download_all()
